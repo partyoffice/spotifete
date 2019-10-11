03093045411e58f84ef0d2166bf7ddf9bb3f86f1
Nikos410 <nikos.epping@web.de>
2019-10-11 14:16:44 +0200

    Implement basic web application using mux

--- spotifete.go
+++ spotifete.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("SpotiFete!")
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/{path}", Any)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func Any(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	path := vars["path"]
+	fmt.Fprintln(w, html.EscapeString(path))
 }
