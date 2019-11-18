d16a11f86498926b5574ffff7e8a728442878b8e
Nikos <nikos.epping@web.de>
2019-11-19 00:25:36 +0100

    Fix listing_sessions FK

---
+++ resources/migrations/000010_modify_listening_sessions_owner_id_fk.down.sql
@@ -0,0 +1,7 @@
+BEGIN;
+ALTER TABLE listening_sessions
+    DROP CONSTRAINT IF EXISTS sessions_owner_id_fkey;
+
+ALTER TABLE listening_sessions
+    ADD CONSTRAINT sessions_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES listening_sessions (id);
+COMMIT;
