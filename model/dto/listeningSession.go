c12d03fbb6233b52ac1bd7816f265c808e18f975
Nikos <nikos.epping@web.de>
2019-12-22 00:54:44 +0100

    Display join id as 2 groups of 4 digits

--- model/dto/listeningSession.go
+++ model/dto/listeningSession.go
@@ -1,5 +1,7 @@
 package dto
 
+import "fmt"
+
 type ListeningSessionDto struct {
 	Owner            UserDto            `json:"owner"`
 	JoinId           string             `json:"joinId"`
@@ -7,4 +9,8 @@ type ListeningSessionDto struct {
 	CurrentlyPlaying *TrackMetadataDto  `json:"currentlyPlaying"`
 	UpNext           *TrackMetadataDto  `json:"upNext"`
 	Queue            []TrackMetadataDto `json:"queue"`
+}
+
+func (listeningSessionDto ListeningSessionDto) GetJoinIdHumanReadable() string {
+	return fmt.Sprintf("%s %s", listeningSessionDto.JoinId[0:4], listeningSessionDto.JoinId[4:8])
 }
