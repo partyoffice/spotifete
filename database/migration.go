74e0470be00cc89ac202b5506f4c84da36dc4313
Nikos410 <nikos.epping@web.de>
2020-07-26 17:45:23 +0200

    Move database migration logic into separate file

---
+++ database/migration.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/google/logger"
+	"github.com/jinzhu/gorm"
+)
+
+func migrateIfNecessary(db *gorm.DB) {
+	logger.Info("Connection aquired. Checking database version")
+	migration := prepareMigration(db)
+	runMigrationIfNecessary(migration)
+}
+
+func prepareMigration(db *gorm.DB) *migrate.Migrate {
+	driver := getDatabaseDriver(db)
+
+	migration, err := migrate.NewWithDatabaseInstance(
+		"file://resources/migrations/",
+		"postgres", driver)
+	if err != nil {
+		logger.Fatalf("could not prepare database migration: %s", err.Error())
+	}
+
+	return migration
+}
+
+func getDatabaseDriver(db *gorm.DB) database.Driver {
+	driver, err := postgres.WithInstance(db.DB(), &postgres.Config{})
+	if err != nil {
+		logger.Fatalf("could not get driver for migration from db instance: %s", err.Error())
+	}
+
+	return driver
+}
+
+func runMigrationIfNecessary(migration *migrate.Migrate) {
+	version, _, _ := migration.Version()
+	if version == targetDatabaseVersion {
+		logger.Infof("Database is up to date! (Version %d)", version)
+	} else {
+		logger.Infof("Database version is %d / target version is %d. Migrating!\n", version, targetDatabaseVersion)
+		runMigration(migration)
+	}
+}
+
+func runMigration(migration *migrate.Migrate) {
+	err := migration.Migrate(targetDatabaseVersion)
+	if err != nil {
+		logger.Fatalf("could not execute migration: %s", err.Error())
+	}
+
+	logger.Info("Migration successful!")
+}
