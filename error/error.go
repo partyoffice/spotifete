c0b4481a928a3bf23b181c32400222e6cf3e77b8
Nikos410 <nikos.epping@web.de>
2020-08-15 13:50:15 +0200

    Allow adding cause to error

--- error/error.go
+++ error/error.go
@@ -4,28 +4,43 @@ import (
 	. "github.com/47-11/spotifete/model/webapp/api/v1"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"reflect"
 )
 
 type SpotifeteError interface {
 	error
 	StringResponse(c *gin.Context)
 	JsonResponse(c *gin.Context)
+	WithCause(cause error) SpotifeteError
+	WithMessage(message string) SpotifeteError
 	getHttpStatus() int
 	shouldShowMessageToUser() bool
+	shouldShowCauseToUser() bool
 	getDefaultMessage() string
 }
 
 type BaseError struct {
 	SpotifeteError
+	Cause   error
 	Message string
 }
 
-func (e BaseError) Error() string {
+func (e BaseError) Error() (errorString string) {
 	if e.shouldShowMessageToUser() {
-		return e.Message
+		errorString = e.Message
 	} else {
-		return e.getDefaultMessage()
+		errorString = e.getDefaultMessage()
 	}
+
+	if e.shouldShowCauseToUser() {
+		errorString += "\n"
+		errorString += "caused by "
+		errorString += reflect.TypeOf(e.Cause).Name()
+		errorString += ": "
+		errorString += e.Cause.Error()
+	}
+
+	return errorString
 }
 
 func (e BaseError) StringResponse(c *gin.Context) {
@@ -36,12 +51,26 @@ func (e BaseError) JsonResponse(c *gin.Context) {
 	c.SecureJSON(e.getHttpStatus(), ErrorResponse{Message: e.Error()})
 }
 
+func (e BaseError) WithMessage(message string) BaseError {
+	e.Message = message
+	return e
+}
+
+func (e BaseError) WithCause(cause error) BaseError {
+	e.Cause = cause
+	return e
+}
+
 func (e BaseError) getHttpStatus() int {
 	return http.StatusInternalServerError
 }
 
 func (e BaseError) shouldShowMessageToUser() bool {
-	return false
+	return true
+}
+
+func (e BaseError) shouldShowCauseToUser() bool {
+	return true
 }
 
 func (e BaseError) getDefaultMessage() string {
