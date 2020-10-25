d518ab818d5758a8e1e9930a092e855789cf5271
Nikos Epping <nikos.epping@web.de>
2020-10-25 21:21:06 +0100

    Remove common.js

--- resources/static/js/common.js
+++
@@ -1,7 +0,0 @@
-if(navigator.userAgent.toLowerCase().indexOf('android') > -1 && document.cookie.indexOf('appDeclined=true') === -1) {
-    if (window.confirm('Hey there!\n\nIt looks like you are using an android device.\n\nDo you want to install our App?')) {
-        window.location.href='/app/android';
-    } else {
-        document.cookie = "appDeclined=true; path=/;";
-    }
-}
