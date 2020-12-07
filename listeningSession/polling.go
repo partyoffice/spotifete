e11d21b0d4834b37610212e3ee8e3c35d5d37556
Nikos Epping <nikos.epping@web.de>
2020-12-07 12:44:58 +0100

    Polling: Log info when polling sessions

--- listeningSession/polling.go
+++ listeningSession/polling.go
@@ -2,6 +2,7 @@ package listeningSession
 
 import (
 	"github.com/47-11/spotifete/database/model"
+	"github.com/google/logger"
 	"time"
 )
 
@@ -11,6 +12,8 @@ func PollSessions() {
 
 func pollSessions() {
 	for range time.Tick(5 * time.Second) {
+		logger.Info("Polling sessions")
+
 		activeSessions := FindFullListeningSessions(model.SimpleListeningSession{
 			Active: true,
 		})
