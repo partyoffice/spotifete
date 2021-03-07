5d5171cdd2f0fe6a8cfd22f0eef764aec9c34e04
Nikos Epping <nikos.epping@protonmail.com>
2021-03-07 17:22:11 +0100

    Add caching for playlist tracks

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/47-11/spotifete
 go 1.16
 
 require (
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/Microsoft/go-winio v0.4.17-0.20210211115548-6eac466e5fa3 // indirect
+	github.com/containerd/containerd v1.5.0-beta.1 // indirect
 	github.com/getsentry/sentry-go v0.10.0
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
@@ -12,17 +13,16 @@ require (
 	github.com/google/logger v1.1.0
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.9.0 // indirect
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/spf13/afero v1.5.1 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.4 // indirect
 	github.com/zmb3/spotify v1.1.1
--- listeningSession/playlist.go
+++ listeningSession/playlist.go
@@ -3,8 +3,15 @@ package listeningSession
 import (
 	"github.com/47-11/spotifete/database"
 	"github.com/47-11/spotifete/database/model"
+	. "github.com/47-11/spotifete/shared"
+	"github.com/47-11/spotifete/users"
+	"github.com/patrickmn/go-cache"
 	"github.com/zmb3/spotify"
+	"net/http"
+	"time"
 )
+
+var playlistTrackCache = cache.New(30*time.Minute, time.Hour)
 
 func AddOrUpdatePlaylistMetadata(playlist spotify.FullPlaylist) model.PlaylistMetadata {
 	knownPlaylistMetadata := GetPlaylistMetadataBySpotifyPlaylistId(playlist.ID.String())
@@ -32,4 +39,55 @@ func GetPlaylistMetadataBySpotifyPlaylistId(playlistId string) *model.PlaylistMe
 	} else {
 		return nil
 	}
+}
+
+func getPlayablePlaylistTracks(playlistId string, user model.SimpleUser) (*[]spotify.FullTrack, *SpotifeteError) {
+	cachedTracks, found := playlistTrackCache.Get(playlistId)
+	if found {
+		return cachedTracks.(*[]spotify.FullTrack), nil
+	}
+
+	loadedTracks, spotifeteError := loadPlaylistTracksFromSpotify(playlistId, user)
+	if spotifeteError != nil {
+		return nil, spotifeteError
+	}
+
+	var playableTracks []spotify.FullTrack
+	for _, playlistTrack := range loadedTracks {
+		track := playlistTrack.Track
+
+		if track.IsPlayable != nil && *track.IsPlayable {
+			playableTracks = append(playableTracks, track)
+		}
+	}
+
+	playlistTrackCache.SetDefault(playlistId, &playableTracks)
+	return &playableTracks, nil
+}
+
+func loadPlaylistTracksFromSpotify(playlistId string, user model.SimpleUser) ([]spotify.PlaylistTrack, *SpotifeteError) {
+	client := users.Client(user)
+
+	spotifyPlaylistId := spotify.ID(playlistId)
+	searchOptions := spotify.Options{Country: &user.Country}
+	var tracks []spotify.PlaylistTrack
+
+	var tracksLeftToLoad = true
+	for tracksLeftToLoad {
+		page, err := client.GetPlaylistTracksOpt(spotifyPlaylistId, &searchOptions, "")
+		if err != nil {
+			return nil, NewError("Could not get playlist tracks from Spotify.", err, http.StatusInternalServerError)
+		}
+
+		tracks = append(tracks, page.Tracks...)
+
+		loadedTrackCount := len(tracks)
+		if loadedTrackCount >= page.Total {
+			tracksLeftToLoad = false
+		} else {
+			searchOptions.Offset = &loadedTrackCount
+		}
+	}
+
+	return tracks, nil
 }
