82a90ba412603d11aafb4c7130f28455301b574d
Nikos Epping <nikos.epping@protonmail.com>
2021-03-07 10:24:53 +0100

    Migration 000006: Delete old listening sessions when migrating up

--- resources/migrations/000006_add_column_owner_id_to_sessions.up.sql
+++ resources/migrations/000006_add_column_owner_id_to_sessions.up.sql
@@ -1,7 +1,12 @@
 BEGIN;
+
+DELETE
+FROM sessions;
+
 ALTER TABLE sessions
     ADD COLUMN owner_id INTEGER NOT NULL DEFAULT -1;;
 
 ALTER TABLE sessions
     ADD CONSTRAINT sessions_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES sessions (id);
+
 COMMIT;
