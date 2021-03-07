50693b37e091c2144ffe2ab3079f1f559d3d4d4d
Nikos Epping <nikos.epping@protonmail.com>
2021-03-07 21:02:08 +0100

    DB migration 000035: Add explainatory comment

--- resources/migrations/000035_add_song_requests_indices.up.sql
+++ resources/migrations/000035_add_song_requests_indices.up.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+-- This index also acts as a unique constraint to ensure that only one
+-- CURRENTLY_PLAYING and one UP_NEXT request are present per session
 CREATE UNIQUE INDEX song_requests_status_index
     ON song_requests (session_id, status)
     WHERE status in ('CURRENTLY_PLAYING', 'UP_NEXT');
